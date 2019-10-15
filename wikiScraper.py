4f50782832fc368ee5db707dee869bd400534576
Dustin Armstrong <dieter@Sandpaws-Macbook-Pro.local>
2019-10-15 13:57:39 -0400

    added output to .txt file

--- wikiScraper.py
+++ wikiScraper.py
@@ -7,4 +7,7 @@ res.raise_for_status()
 # to raise the status code if any error occurs
 wiki = bs4.BeautifulSoup(res.text, "html.parser")
 for i in wiki.select('p'):
-    print(i.getText())
+    # append scraped data to .txt file
+    sample = open('samplefile.txt', 'a')
+    print(i.getText(), file = sample)
+    sample.close()
